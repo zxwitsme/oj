0e78c26c56a2ec6398eb53a4100bfd2afdfcbbe5
zhouxuewen <zhouxuewen@xiaomi.com>
2018-01-18 15:44:54 +0800

    update 32
    still not find the root cause for running error
    
    Signed-off-by: zhouxuewen <zhouxuewen@xiaomi.com>

--- 32/max_sum_fast.c
+++ 32/max_sum_fast.c
@@ -5,7 +5,7 @@
 void solution(char *line)
 {
 	char *line_a, *saveptr;
-	int s[512], lr[256][32], num[512];
+	int s[512], lr[256][2], num[512];
 	int i, j, temp, max = 0;
 	int q = 0, m = 0, n = 0;
 
@@ -30,10 +30,8 @@ void solution(char *line)
 		m = 0;
 		while (1) {
 			j = atoi(line_a);
-			if (m >= 2) {
-				printf("invalid m\n");
-				break;
-			}
+			if (m >= 2)
+				printf("m too large\n");
 			lr[i][m++] = j;
 			line_a = strstr(line_a, " ");
 			if (!line_a)
@@ -44,31 +42,29 @@ void solution(char *line)
 	}
 
 	for (j = 0; j < n; j++) {
-		for (i = 0; i < n - j; i++) {
-			if (s[i] < s[i+1]) {
+		for (i = n - 1; i > j; i--) {
+			if (s[i-1] < s[i]) {
 				temp = s[i];
-				s[i] = s[i+1];
-				s[i+1] = temp;
+				s[i] = s[i-1];
+				s[i-1] = temp;
 			}
 		}
 	}
 
 	for (i = 0; i < q; i++) {
 		for (j = lr[i][0] - 1; j < lr[i][1]; j++) {
-			if (j < 0 || j >= n) {
-				printf("invalid j %d\n", j);
-				continue;
-			}
+			if (j < 0)
+				printf("j < 0\n");
 			num[j]++;
 		}
 	}
 
 	for (j = 0; j < n; j++) {
-		for (i = 0; i < n - j; i++) {
-			if (num[i] < num[i+1]) {
+		for (i = n - 1; i > j; i--) {
+			if (num[i-1] < num[i]) {
 				temp = num[i];
-				num[i] = num[i+1];
-				num[i+1] = temp;
+				num[i] = num[i-1];
+				num[i-1] = temp;
 			}
 		}
 	}
