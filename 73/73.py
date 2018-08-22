7ebd6983f3009429c95a4e82ec73d70561e20f14
zhouxuewen <zhouxuewen@xiaomi.com>
2018-08-22 17:37:04 +0800

    add 73
    Signed-off-by: zhouxuewen <zhouxuewen@xiaomi.com>

--- 73/73.py
+++ 73/73.py
@@ -1,12 +1,25 @@
 def solution(line):
-    l = len(line)
-    print(l)
-    c = 'a'
-    for i in range(l)
-        if line[i] == '?':
-            line[i] = c;
+  l = len(line)
+  result = 0
+  count = 0
+  for i in range(l):
+    if line[i] == '?':
+      count += 1
+    else:
+      if i > 0:
+        if line[i-1] != '?' and line[i] == line[i-1]:
+          result += 1
+      if count == 0:
+        continue
+      if count%2 == 0 and i-count-1 >= 0:
+        if line[i-count-1] == line[i]:
+          result += 1
+      elif i-count-1 >= 0:
+        if line[i-count-1] != line[i]:
+          result += 1
+      count = 0
 
-    return l
+  return result
 
 if __name__ == '__main__':
     y = raw_input()
